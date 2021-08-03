095b81fb624a8f982d1a1b1378720661c03d5c53
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2021-08-03 20:20:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server, { cors: true, origins: ["https://610955f6ade53de4413d6b04--friendly-leakey-b5b21d.netlify.app/"] });
+const io = socketio(server, { cors: true, origins: ["https://friendly-leakey-b5b21d.netlify.app/"] });
 
 app.use(cors());
 app.use(router);
